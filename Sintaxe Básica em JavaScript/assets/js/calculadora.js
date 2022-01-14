8ebf6bab97e2792dcb9ba418644633ff1090a075
BrunoMundim <brunomundimfranco@gmail.com>
2022-01-14 13:05:26 -0300

    Correção Sintaxe Básica em JavaScript

--- Sintaxe Básica em JavaScript/assets/js/calculadora.js
+++ Sintaxe Básica em JavaScript/assets/js/calculadora.js
@@ -10,68 +10,67 @@ function calculadora(){
         alert('Operação inválida!');
         calculadora();
     } else{
+        let n1 = Number(prompt('Insira o primeiro valor:'));
+        let n2 = Number(prompt('Insira o segundo valor:'));
+        let resultado;
 
-    let n1 = Number(prompt('Insira o primeiro valor:'));
-    let n2 = Number(prompt('Insira o segundo valor:'));
-    let resultado;
-
-    if (!n1 || !n2){
-        alert('Parâmetros inválidos!')
-        calculadora();
-    } else{
-
-    function soma() {
-        resultado = n1 + n2;
-        alert(`${n1} + ${n2} = ${resultado}`);
-    }
-    function subtracao() {
-        resultado = n1 - n2;
-        alert(`${n1} - ${n2} = ${resultado}`);
-    }
-    function multiplicacao() {
-        resultado = n1 * n2;
-        alert(`${n1} * ${n2} = ${resultado}`);
-    }
-    function divisaoReal() {
-        resultado = n1 / n2;
-        alert(`${n1} / ${n2} = ${resultado}`);
-    }
-    function divisaoInteira() {
-        resultado = n1 % n2;
-        alert(`${n1} % ${n2} = ${resultado}`);
-    }
-    function potenciacao() {
-        resultado = n1 ** n2;
-        alert(`${n1} ** ${n2} = ${resultado}`);
-    }
-
-    function novaOperacao(){
-        let opcao = Number(prompt('Deseja fazer outra operação? \n 1 - Sim\n 2 - Não'));
-        if (opcao == 1){
+        if (!n1 || !n2){
+            alert('Parâmetros inválidos!')
             calculadora();
-        } else if (opcao == 2) {
-            alert('Até mais');
-        } else {
-            alert ('Digite uma opção válida')
+        } else{
+
+            function soma() {
+                resultado = n1 + n2;
+                alert(`${n1} + ${n2} = ${resultado}`);
+            }
+            function subtracao() {
+                resultado = n1 - n2;
+                alert(`${n1} - ${n2} = ${resultado}`);
+            }
+            function multiplicacao() {
+                resultado = n1 * n2;
+                alert(`${n1} * ${n2} = ${resultado}`);
+            }
+            function divisaoReal() {
+                resultado = n1 / n2;
+                alert(`${n1} / ${n2} = ${resultado}`);
+            }
+            function divisaoInteira() {
+                resultado = n1 % n2;
+                alert(`${n1} % ${n2} = ${resultado}`);
+            }
+            function potenciacao() {
+                resultado = n1 ** n2;
+                alert(`${n1} ** ${n2} = ${resultado}`);
+            }
+
+            function novaOperacao(){
+                let opcao = Number(prompt('Deseja fazer outra operação? \n 1 - Sim\n 2 - Não'));
+                if (opcao == 1){
+                    calculadora();
+                } else if (opcao == 2) {
+                    alert('Até mais');
+                } else {
+                    alert ('Digite uma opção válida')
+                    novaOperacao();
+                }
+            }
+
+            if (operacao == 1) {
+                soma();
+            } else if (operacao == 2) {
+                subtracao();
+            } else if(operacao == 3){
+                multiplicacao();
+            } else if (operacao == 4) {
+                divisaoReal();
+            } else if (operacao == 5) {
+            divisaoInteira();
+            } else if (operacao == 6) {
+                potenciacao();
+            }
             novaOperacao();
         }
-    }
-
-    if (operacao == 1) {
-        soma();
-    } else if (operacao == 2) {
-        subtracao();
-    } else if(operacao == 3){
-        multiplicacao();
-    } else if (operacao == 4) {
-        divisaoReal();
-    } else if (operacao == 5) {
-       divisaoInteira();
-    } else if (operacao == 6) {
-        potenciacao();
-    }
-    novaOperacao();
-    }
     }
 }
 
